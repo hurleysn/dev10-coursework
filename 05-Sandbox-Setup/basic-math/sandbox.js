46aac99ff8acccacce929bd6779ed2e5beadafe1
Sean <hurleysn@bc.edu>
2022-05-18 12:18:31 -0400

    commoiy

--- 05-Sandbox-Setup/basic-math/sandbox.js
+++ 05-Sandbox-Setup/basic-math/sandbox.js
@@ -16,3 +16,13 @@ console.log(`${x} / ${y} is ${val}`);
 y = 3;
 val = x % y;
 console.log(`${x} % ${y} is ${val}`);
+
+let z = 3
+val = x * y - z
+console.log(`${x} * ${y} - ${z} is ${val}`)
+
+val = x - y * z
+console.log(`${x} - ${y} * ${z} is ${val}`)
+
+val = (x - y) * z
+console.log(`(${x} - ${y}) * ${z} is ${val}`)
